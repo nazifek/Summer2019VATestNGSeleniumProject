b4edc1d31649905a7e288606560b97dc209db018
nazife <nazifekorkut@gmail.com>
2019-10-28 15:09:14 -0400

    class

---
+++ tests/d4_basic_locators/IdLocatorTest.java
@@ -0,0 +1,20 @@
+package com.cybertek.tests.d4_basic_locators;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class IdLocatorTest {
+    public static void main(String[] args) {
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        WebElement element=driver.findElement(By.id("disappearing_button"));
+        String txt = element.getText();
+        element.click();
+        driver.quit();
+
+
+    }
+}
